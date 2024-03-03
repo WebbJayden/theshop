b7ed2555a29b6b59936969c4e26350bbf3e2760d
WebbJayden <jaydenwebbcv@gmail.com>
2024-03-03 20:12:07 +0000

    created shopkeep and DialogueManager scenes. Plan with DIalogueManager is to create Key/Value paris for each character that will speak, key is their name adn values are dialogue obvs lolol. we can then reference this in StartDialogue methods that I sitll need to create by doing if checks or somethigne lse if mty brain is big. cool

---
+++ scenes/DialogueManager.cs
@@ -0,0 +1,14 @@
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class DialogueManager : Control
+{
+    // create dictionary to contain dialogue for shopkeep
+    private Dictionary<string, string[]> dialogues = new Dictionary<string, string[]>
+    {
+        // initial key string
+        // create 'new string' for value(s)
+        { "Shopkeep", new string[] { "welcome.", "you new around here?", "nice to see a new face every now and again.", "sometimes there's strange noises from that room back there. i wouldn't worry about it heh heh heh."} }
+    }
+}
---
+++ scripts/Shopkeep.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Shopkeep : CharacterBody2D
+{
+    public void _on_body_entered()
+    { 
+        
+    }
+}

bfdeed8d0f66f0fe8aa8754c29dc6490f376403f
WebbJayden <91128093+WebbJayden@users.noreply.github.com>
2024-03-18 15:13:19 +0000

    added collision detection to ScenManager, can now interact with player. need to add scene change function

--- scripts/SceneManager.cs
+++ scripts/SceneManager.cs
@@ -1,6 +1,22 @@
 using Godot;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class SceneManager : Area2D
 {
+    // get the current scene property
+    public Node CurrentScene { get; set; }
+
+    // get/set next scene property
+    public Node NextScene { get; set; }
+
+    // Collision detection
+    public void _on_scenemanager_entered(CharacterBody2D body)
+    {
+        GD.Print($"Scene Manager entered: {body}");
+    }
+
+    // if player enters SceneCollision, change scene
+
+
 }
